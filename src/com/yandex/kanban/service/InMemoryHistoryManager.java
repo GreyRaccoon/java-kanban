a3c95d126a0d2e2f5cba35726a9f61a591416796
Timofey Fernaty <GreyRaccoon@ya.ru>
2025-08-16 20:44:12 +0300

    Sprint 6: InMemoryHistoryManager.remove() fix

--- src/com/yandex/kanban/service/InMemoryHistoryManager.java
+++ src/com/yandex/kanban/service/InMemoryHistoryManager.java
@@ -43,8 +43,8 @@ public class InMemoryHistoryManager implements HistoryManager {
     @Override
     public void remove(int id) {
         Node node = historyMap.get(id);
+        removeNode(node);
         if (node != null) {
-            removeNode(node);
             historyMap.remove(id);
         }
     }
